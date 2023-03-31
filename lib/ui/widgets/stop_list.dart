a1f4bb067acc35f6eac39e1a777ab857872ef7c1
Kuoppala Siiri <22868831+siiruli@users.noreply.github.com>
2023-03-31 21:25:19 +0300

    Show next departures

--- lib/ui/widgets/stop_list.dart
+++ lib/ui/widgets/stop_list.dart
@@ -25,10 +25,30 @@ class StopListWidget extends StatelessWidget {
     );
   }
 }
+Widget getDepartureTime(int? time){
+  String timeStr = time?.toString() ?? "--";
+  return Text(timeStr);
+}
+class StopTimeWidget extends StatelessWidget {
+  const StopTimeWidget({this.stoptime, Key? key}) : super(key: key);
+  final Query$StopData$stops$stoptimesWithoutPatterns? stoptime;
 
-
+  @override
+  Widget build(BuildContext context){
+    if(stoptime == null) {
+      return Text("null");
+    }
+    String busName = stoptime?.trip?.route.shortName ?? "unnamed";
+    busName += ' ' + (stoptime?.headsign ?? "-");
+    return Card(
+      child: ListTile(
+        title: Text(busName),
+        leading: getDepartureTime(stoptime?.realtimeDeparture),
+      ),
+    );
+  }
+}
 Widget stopItemBuilder(Query$StopData$stops? stop){
-
   if(stop == null){
     return const Text("Stop is null");
   }
@@ -53,11 +73,17 @@ Widget stopItemBuilder(Query$StopData$stops? stop){
     }
   );
 
+  List<Widget>? stopTimes = stop.stoptimesWithoutPatterns?.map(
+    (e) => StopTimeWidget(stoptime: e)
+  ).toList();
+
   return Card(
-    child: ListTile(
+    child: ExpansionTile(
       title: Text(stop.name),
       subtitle: stop.code == null ? null : Text(stop.code!),
       trailing: iconButton,
+      children: stopTimes ?? <Widget>[const Text("No departures")],
+      controlAffinity: ListTileControlAffinity.leading,
     )
   );
 }
