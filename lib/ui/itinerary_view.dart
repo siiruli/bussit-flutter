c27bb3bd87857f6eb04fe777ca74591eb1aa2601
Kuoppala Siiri <22868831+siiruli@users.noreply.github.com>
2023-04-07 17:17:07 +0300

    Add buttons to itinerary form

--- lib/ui/itinerary_view.dart
+++ lib/ui/itinerary_view.dart
@@ -4,6 +4,8 @@ import 'package:bussit/api/location_api.dart';
 import 'package:bussit/model/address.dart';
 import 'package:bussit/ui/widgets/itineraries/itinerary_list.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as developer;
+
 
 class ItineraryView extends StatelessWidget {
   const ItineraryView({Key? key}) : super(key: key);
@@ -25,9 +27,14 @@ class _ItineraryFormState extends State<ItineraryForm>  with AutomaticKeepAliveC
 
   final _formKey = GlobalKey<FormState>();  
   Address? _locationFrom, _locationTo;
-  
+  bool _arriveBy = false;
   Widget? _result;
-  
+  DateTime? _date;
+  DateTime? _time;
+
+  Key _keyFrom = GlobalKey();
+  Key _keyTo = GlobalKey();
+
   @override
   bool get wantKeepAlive => true;
 
@@ -53,26 +60,79 @@ class _ItineraryFormState extends State<ItineraryForm>  with AutomaticKeepAliveC
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    // List stops that is rebuilt when the id-list changes
+
+    // places
+    final fieldFrom = LocationField(
+      key: _keyFrom,
+      hint: "From...",
+      onSaved: (Address? value){_locationFrom = value;},
+    );
+    final fieldTo = LocationField(
+      key: _keyTo,
+      hint: "To...",
+      onSaved: (Address? value){_locationTo = value;},
+    );
+    final swapButton = IconButton(
+      icon: const Icon(Icons.swap_vert),
+      onPressed: (){
+        // Swap from and to
+        _formKey.currentState?.save();
+        setState(() {
+          final tmp = _keyFrom;
+          _keyFrom = _keyTo;
+          _keyTo = tmp;
+        });
+      },
+    );
+    final places = Row(children: [
+      Expanded(child: Column(children: [
+        fieldFrom, fieldTo
+      ],)),
+      swapButton,
+    ],);
+    // time
+    const dateChooser = Text('Date');
+    const timeChooser = Text('Time');
+    final nowButton = TextButton(
+      onPressed: () => setState(() {
+        _date = null;
+        _time = null;
+      }), 
+      child: const Text('Now'),
+    );
+    final arrivalOrDeparture = DropdownButton<bool>(
+      value: _arriveBy,
+      items: const [
+        DropdownMenuItem(value: false, child: Text("Departure")),
+        DropdownMenuItem(value: true, child: Text("Arrival")),
+      ], 
+      onChanged: (bool? value) {
+        setState(() {
+          _arriveBy = value ?? _arriveBy;
+        });
+      }
+    );
+    
+
     final form = Padding(
       padding: const EdgeInsets.all(8), 
       child: Form(
         key: _formKey,
-        child: Column(children: [
-          LocationField(
-            hint: "From...",
-            initialValue: _locationFrom,
-            onSaved: (Address? value){_locationFrom = value;},
-          ),
-          LocationField(
-            hint: "To...",
-            onSaved: (Address? value){_locationTo = value;},
-            initialValue: _locationTo,
-          ),
-          TextButton(
-            onPressed: saveResult,
-            child: const Text("Search routes"),
-          ),
+        child: Column(
+          children: [places, 
+          Row(
+            children: [
+              arrivalOrDeparture,
+              dateChooser,
+              timeChooser,
+              nowButton, 
+              TextButton(
+                onPressed: saveResult,
+                child: const Text("Search routes"),
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          ),          
         ]),
       ),
     );
@@ -96,17 +156,14 @@ String autocompleteOptionString(Address feature){
 class LocationField extends StatelessWidget {
   const LocationField({
     this.hint, this.onSaved, Key? key,
-    this.initialValue,
   }) : super(key: key);
 
   final String? hint;
   final Function(Address?)? onSaved;
-  final Address? initialValue;
   @override
   Widget build(BuildContext context) {
     return FormField<Address?>(
       onSaved: onSaved,
-      initialValue: initialValue,
       validator: (value){return value == null ? "Missing location" : null;},
       builder: (FormFieldState<Address?> field){
         return Autocomplete<Address>(
@@ -118,6 +175,9 @@ class LocationField extends StatelessWidget {
           },
           fieldViewBuilder: (context, textEditingController, focusNode, onFieldSubmitted){
             return TextFormField(
+              validator: (value){
+                return (field.value == null) ? "required" : null;
+              },
               focusNode: focusNode,
               controller: textEditingController,
               decoration: InputDecoration(hintText: hint),
