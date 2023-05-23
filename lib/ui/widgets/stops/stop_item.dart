b80f161fd3d64f107759b7504d5da93dbeb11370
Kuoppala Siiri <22868831+siiruli@users.noreply.github.com>
2023-05-23 13:29:22 +0300

    Use dividers instead of cards in stoptime list

--- lib/ui/widgets/stops/stop_item.dart
+++ lib/ui/widgets/stops/stop_item.dart
@@ -1,4 +1,3 @@
-
 import 'package:bussit/ui/widgets/components/app_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -6,79 +5,80 @@ import 'package:bussit/graphql/stops_query.graphql.dart';
 import 'package:bussit/model/saved_stops.dart';
 import 'package:bussit/ui/widgets/components/departure_time.dart';
 
-
 class ExpansionStopItem extends StatelessWidget {
   const ExpansionStopItem(this.stopMaybe, {Key? key}) : super(key: key);
   final dynamic stopMaybe;
 
   @override
-  Widget build(BuildContext context){
-    if(stopMaybe == null){
+  Widget build(BuildContext context) {
+    if (stopMaybe == null) {
       return const Text("Stop is null");
     }
-    if((stopMaybe !is Query$StopData$stations?) && 
-        (stopMaybe !is Query$StopData$stops?)
-    ){
+    if ((stopMaybe! is Query$StopData$stations?) &&
+        (stopMaybe! is Query$StopData$stops?)) {
       return const Text("Wrong type");
     }
     final stop = stopMaybe!;
 
     List? stopTimeData = stop.stoptimesWithoutPatterns;
-    List<Widget> stopTimes = stopTimeData?.map(
-      (e) => StopTimeWidget(e)
-    ).toList() ?? <Widget>[];
-    
-    if(stopTimes.isEmpty){
+    List<Widget> stopTimes =
+        stopTimeData?.map((e) => StopTimeWidget(e)).toList() ?? <Widget>[];
+
+    if (stopTimes.isEmpty) {
       stopTimes = <Widget>[
-        const Card(child: ListTile(
+        const Card(
+            child: ListTile(
           title: Text("No departures"),
         ))
       ];
     }
 
-    return Card(child: ExpansionTile(
+    return Card(
+        child: ExpansionTile(
       childrenPadding: EdgeInsets.zero,
-      title: StopItemWidget(stopMaybe, padding: EdgeInsets.zero,),
-      children: stopTimes,
+      title: StopItemWidget(
+        stopMaybe,
+        padding: EdgeInsets.zero,
+      ),
+      children:
+          ListTile.divideTiles(context: context, tiles: stopTimes).toList(),
       controlAffinity: ListTileControlAffinity.leading,
     ));
   }
 }
+
 class StopItemWidget extends StatelessWidget {
   const StopItemWidget(this.stopMaybe, {this.padding, super.key});
   final dynamic stopMaybe;
   final EdgeInsets? padding;
 
   @override
-  Widget build(BuildContext context){
-    if(stopMaybe == null){
+  Widget build(BuildContext context) {
+    if (stopMaybe == null) {
       return const Text("Stop is null");
     }
     final stop = stopMaybe!;
 
-    final iconButton = getSaveIconButton(stop);    
+    final iconButton = getSaveIconButton(stop);
     List<InlineSpan> titleText = [];
-    if(stop.platformCode != null) {
+    if (stop.platformCode != null) {
       titleText.add(TextSpan(
         text: ' ' + stop.platformCode!,
       ));
     }
     List<String?> subtitleTexts = [
-      (stop is Query$StopData$stations)? "Station" : stop.code,
+      (stop is Query$StopData$stations) ? "Station" : stop.code,
       stop.desc,
     ];
     subtitleTexts = subtitleTexts.where((element) => element != null).toList();
-    List<Widget> subtitle = subtitleTexts.map((e) => Text(e! + ' ')).toList(); 
-    
+    List<Widget> subtitle = subtitleTexts.map((e) => Text(e! + ' ')).toList();
+
     return ListTile(
-      title: Text.rich(
-        TextSpan(text: stop.name, 
-        children: titleText
-      )),
+      title: Text.rich(TextSpan(text: stop.name, children: titleText)),
       subtitle: Row(children: subtitle),
       trailing: Row(
         children: <Widget>[
-          TransitModeIcon(stop.vehicleMode), 
+          TransitModeIcon(stop.vehicleMode),
           ZoneIdIcon(stop.zoneId),
           iconButton,
         ],
@@ -94,47 +94,43 @@ class StopTimeWidget extends StatelessWidget {
   final dynamic stoptime;
 
   @override
-  Widget build(BuildContext context){
-    if(stoptime == null) {
+  Widget build(BuildContext context) {
+    if (stoptime == null) {
       return const Text("null");
     }
     String busName = stoptime?.trip?.route.shortName ?? "unnamed";
     busName += ' ' + (stoptime?.headsign ?? "-");
     Widget? depTime = DepartureTimeWidget(
-      seconds: stoptime?.serviceDay + stoptime?.realtimeDeparture, 
+      seconds: stoptime?.serviceDay + stoptime?.realtimeDeparture,
       isRealTime: stoptime?.realtime,
     );
     final platFormCode = stoptime?.stop?.platformCode;
     final code = (platFormCode == null) ? null : Text(platFormCode);
-    return Card(
-      child: ListTile(
-        title: Text(busName),
-        leading: depTime,
-        trailing: code,
-      ),
+    return ListTile(
+      title: Text(busName),
+      leading: depTime,
+      trailing: code,
+      visualDensity: VisualDensity.compact,
     );
   }
 }
 
-
-Widget getSaveIconButton(stop){
-const saveIcon = Icon(Icons.save_alt);
+Widget getSaveIconButton(stop) {
+  const saveIcon = Icon(Icons.save_alt);
   const deleteIcon = Icon(Icons.delete_forever);
-  final iconButton = Consumer<SavedStopIds>(
-    builder: (context, savedIds, child) {
-      if(savedIds.isSaved(stop.gtfsId)){
-        return IconButton(
-          onPressed: () => savedIds.remove(stop.gtfsId), 
-          icon: deleteIcon,
-        );
-      }
-      else{
-        return IconButton(
-          onPressed: () => savedIds.add(stop.gtfsId), 
-          icon: saveIcon,
-        );
-      }
+  final iconButton =
+      Consumer<SavedStopIds>(builder: (context, savedIds, child) {
+    if (savedIds.isSaved(stop.gtfsId)) {
+      return IconButton(
+        onPressed: () => savedIds.remove(stop.gtfsId),
+        icon: deleteIcon,
+      );
+    } else {
+      return IconButton(
+        onPressed: () => savedIds.add(stop.gtfsId),
+        icon: saveIcon,
+      );
     }
-  );
+  });
   return iconButton;
 }
